2fe12940a424efed6484e0c7dcca13a8875ed082
hujia7812556 <hujia7812556@gmail.com>
2015-04-14 18:03:54 +0800

    增加文章列表

--- app/routes.php
+++ app/routes.php
@@ -12,7 +12,9 @@
 */
 
 Route::get('/', function () {
-    return View::make('index');
+    $articles = Article::with('user', 'tags')->orderBy('created_at', 'desc')->paginate(10);
+    $tags = Tag::where('count', '>', '0')->orderBy('count', 'desc')->orderBy('updated_at', 'desc')->take(10)->get();
+    return View::make('index')->with('articles', $articles)->with('tags', $tags);
 });
 Route::get('login', function () {
     return View::make('login');
@@ -39,35 +41,31 @@ Route::post('login', array('before' => 'csrf', function () {
     }
 }));
 
-Route::get('home', array('before' => 'auth', function () {
-    return View::make('home');
+Route::get('home', array('before' => 'auth', function()
+{
+    return View::make('home')->with('user', Auth::user())->with('articles', Article::with('tags')->where('user_id', '=', Auth::id())->orderBy('created_at', 'desc')->get());
 }));
 
-Route::get('logout', array('before' => 'auth', function()
-{
+Route::get('logout', array('before' => 'auth', function () {
     Auth::logout();
     return Redirect::to('/');
 }));
 
-Route::get('register', function()
-{
+Route::get('register', function () {
     return View::make('users.create');
 });
 
-Route::post('register', array('before' => 'csrf', function()
-{
+Route::post('register', array('before' => 'csrf', function () {
     $rules = array(
         'email' => 'required|email|unique:users,email',
         'nickname' => 'required|min:4|unique:users,nickname',
         'password' => 'required|min:6|confirmed',
     );
     $validator = Validator::make(Input::all(), $rules);
-    if ($validator->passes())
-    {
+    if ($validator->passes()) {
         $user = User::create(Input::only('email', 'password', 'nickname'));
         $user->password = Hash::make(Input::get('password'));
-        if ($user->save())
-        {
+        if ($user->save()) {
             return Redirect::to('login')->with('message', array('type' => 'success', 'content' => 'Register successfully, please login'));
         } else {
             return Redirect::to('register')->withInput()->with('message', array('type' => 'danger', 'content' => 'Register failed'));
@@ -77,8 +75,7 @@ Route::post('register', array('before' => 'csrf', function()
     }
 }));
 
-Route::get('user/{id}/edit', array('before' => 'auth', 'as' => 'user.edit', function($id)
-{
+Route::get('user/{id}/edit', array('before' => 'auth', 'as' => 'user.edit', function ($id) {
     if (Auth::user()->is_admin or Auth::id() == $id) {
         return View::make('users.edit')->with('user', User::find($id));
     } else {
@@ -86,21 +83,18 @@ Route::get('user/{id}/edit', array('before' => 'auth', 'as' => 'user.edit', func
     }
 }));
 
-Route::put('user/{id}', array('before' => 'auth|csrf', function($id)
-{
+Route::put('user/{id}', array('before' => 'auth|csrf', function ($id) {
     if (Auth::user()->is_admin or (Auth::id() == $id)) {
         $user = User::find($id);
         $rules = array(
             'password' => 'required_with:old_password|min:6|confirmed',
             'old_password' => 'min:6',
         );
-        if (!(Input::get('nickname') == $user->nickname))
-        {
+        if (!(Input::get('nickname') == $user->nickname)) {
             $rules['nickname'] = 'required|min:4||unique:users,nickname';
         }
         $validator = Validator::make(Input::all(), $rules);
-        if ($validator->passes())
-        {
+        if ($validator->passes()) {
             if (!(Input::get('old_password') == '')) {
                 if (!Hash::check(Input::get('old_password'), $user->password)) {
                     return Redirect::route('user.edit', $id)->with('user', $user)->with('message', array('type' => 'danger', 'content' => 'Old password error'));
@@ -119,34 +113,28 @@ Route::put('user/{id}', array('before' => 'auth|csrf', function($id)
     }
 }));
 
-Route::group(array('prefix' => 'admin', 'before' => 'auth|isAdmin'), function()
-{
-    Route::get('users', function()
-    {
+Route::group(array('prefix' => 'admin', 'before' => 'auth|isAdmin'), function () {
+    Route::get('users', function () {
         return View::make('admin.users.list')->with('users', User::all())->with('page', 'users');
     });
 });
 
 Route::model('user', 'User');
 
-Route::group(array('before' => 'auth|csrf|isAdmin'), function()
-{
-    Route::put('user/{user}/reset', function(User $user)
-    {
+Route::group(array('before' => 'auth|csrf|isAdmin'), function () {
+    Route::put('user/{user}/reset', function (User $user) {
         $user->password = Hash::make('123456');
         $user->save();
         return Redirect::to('admin/users')->with('message', array('type' => 'success', 'content' => 'Reset password successfully'));
     });
 
-    Route::delete('user/{user}', function(User $user)
-    {
+    Route::delete('user/{user}', function (User $user) {
         $user->block = 1;
         $user->save();
         return Redirect::to('admin/users')->with('message', array('type' => 'success', 'content' => 'Lock user successfully'));
     });
 
-    Route::put('user/{user}/unblock', function(User $user)
-    {
+    Route::put('user/{user}/unblock', function (User $user) {
         $user->block = 0;
         $user->save();
         return Redirect::to('admin/users')->with('message', array('type' => 'success', 'content' => 'Unlock user successfully'));
@@ -155,3 +143,4 @@ Route::group(array('before' => 'auth|csrf|isAdmin'), function()
 
 Route::resource('article', 'ArticleController');
 Route::post('article/preview', array('before' => 'auth', 'uses' => 'ArticleController@preview'));
+Route::get('user/{user}/articles', 'UserController@articles');
