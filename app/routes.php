148ad56b6fcf5b39e5d9b56f36a30f1455ed95ab
hujia7812556 <hujia7812556@gmail.com>
2015-04-14 10:44:32 +0800

    实现用户注册

--- app/routes.php
+++ app/routes.php
@@ -32,7 +32,7 @@ Route::post('login', array('before' => 'csrf', function () {
         ) {
             return Redirect::intended('home');
         } else {
-            return Redirect::to('login')->withInput()->with('message', 'E-mail or password error');
+            return Redirect::to('login')->withInput()->with('message', array('type' => 'danger', 'content' => 'E-mail or password error'));
         }
     } else {
         return Redirect::to('login')->withInput()->withErrors($validator);
@@ -47,4 +47,32 @@ Route::get('logout', array('before' => 'auth', function()
 {
     Auth::logout();
     return Redirect::to('/');
+}));
+
+Route::get('register', function()
+{
+    return View::make('users.create');
+});
+
+Route::post('register', array('before' => 'csrf', function()
+{
+    $rules = array(
+        'email' => 'required|email|unique:users,email',
+        'nickname' => 'required|min:4|unique:users,nickname',
+        'password' => 'required|min:6|confirmed',
+    );
+    $validator = Validator::make(Input::all(), $rules);
+    if ($validator->passes())
+    {
+        $user = User::create(Input::only('email', 'password', 'nickname'));
+        $user->password = Hash::make(Input::get('password'));
+        if ($user->save())
+        {
+            return Redirect::to('login')->with('message', array('type' => 'success', 'content' => 'Register successfully, please login'));
+        } else {
+            return Redirect::to('register')->withInput()->with('message', array('type' => 'danger', 'content' => 'Register failed'));
+        }
+    } else {
+        return Redirect::to('register')->withInput()->withErrors($validator);
+    }
 }));
