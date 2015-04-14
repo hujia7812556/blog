559aa8d353687168213efa28de3294af2de85fc7
hujia7812556 <hujia7812556@gmail.com>
2015-04-14 19:11:02 +0800

    实现文章编辑和删除

--- app/routes.php
+++ app/routes.php
@@ -144,3 +144,4 @@ Route::group(array('before' => 'auth|csrf|isAdmin'), function () {
 Route::resource('article', 'ArticleController');
 Route::post('article/preview', array('before' => 'auth', 'uses' => 'ArticleController@preview'));
 Route::get('user/{user}/articles', 'UserController@articles');
+Route::post('article/{id}/preview', array('before' => 'auth', 'uses' => 'ArticleController@preview'));
