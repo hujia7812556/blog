d0f345216640967b86511120458f09e074564f53
jiahu <jiahu@pptv.com>
2015-04-24 18:18:38 +0800

    迁移语言包

---
+++ resources/lang/en/message.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiahu
+ * Date: 2015/4/20
+ * Time: 11:39
+ */
+return array(
+    'yes' => 'yes',
+    'no' => 'no',
+    'editarticle' => 'Edit Article',
+    'edittag' => 'Edit Tag',
+
+    'index.by' => 'by',
+    'index.postedon' => 'posted on',
+    'index.under' => 'under',
+
+    'topbar.name' =>'HuJia Blog',
+    'topbar.users' => 'Users',
+    'topbar.articles' => 'Articles',
+    'topbar.tags' => 'Tags',
+    'topbar.login' => 'Login',
+    'topbar.logout' => 'Exit',
+    'topbar.register' => 'Register',
+    'topbar.publisharticle' => 'Publish Article',
+    'topbar.information' => 'Information',
+    'topbar.myarticles' => 'My Articles',
+
+    'user.edit.email'=>'E-mail:',
+    'user.edit.nickname'=>'NickName:',
+    'user.edit.oldpassword'=>'OldPassword:',
+    'user.edit.newpassword'=>'NewPassword:',
+    'user.edit.confirmpassword'=>'ConfirmPassword:',
+    'user.edit.modify'=>'Modify',
+    'user.edit.modify.success'=>'Modify successfully',
+
+    'login.email' => 'E-mail:',
+    'login.password' => 'Password:',
+    'login.rememberme  ' => 'Remember Me',
+    'login.login' => 'Login',
+
+    'register.email'=>'E-mail:',
+    'register.nickname'=>'NickName:',
+    'register.password'=>'Password:',
+    'register.confirmpassword'=>'ConfirmPassword:',
+    'register.register'=>'Register',
+    'register.successs'=>'Register successfully, please login',
+    'register.failure'=>'Register failed',
+
+    'admin.users.id' => 'ID',
+    'admin.users.email' => 'E-mail',
+    'admin.users.nickname' => 'Nickname',
+    'admin.users.management' => 'Management',
+    'admin.users.edit' => 'Edit',
+    'admin.users.reset' => 'Reset',
+    'admin.users.reset.success' => 'Reset password successfully',
+    'admin.users.block' => 'Block',
+    'admin.users.block.success' => 'Lock user successfully',
+    'admin.users.unblock' => 'unBlock',
+    'admin.users.unblock.success' => 'Unlock user successfully',
+    'admin.users.reset.sure' => 'Sure you want to reset the password for 123456?',
+    'admin.users.lock.sure' => 'Sure you want to lock it?',
+    'admin.users.unlock.sure' => 'Sure you want to unlock it?',
+
+    'articles.title' => 'Title',
+    'articles.tags' => 'Tags',
+    'articles.tags.hint' => 'Separate multiple tags with a comma ","',
+    'articles.content' => 'Content',
+    'articles.management' => 'Managment',
+    'articles.edit' => 'Edit',
+    'articles.delete' => 'Delete',
+    'articles.delete.sure' => 'Sure you want to delete it?',
+    'articles.preview' => 'Preview',
+    'articles.modify' => 'Modify',
+    'articles.publish' => 'Publish',
+    'articles.author' => 'Author',
+    'articles.datetime' => 'Datetime',
+
+    'tags' => 'Tags',
+    'tags.alltags' => 'All Tags',
+    'tags.tagname' => 'TagName',
+    'tags.articlecount' => 'ArticleCount',
+    'tags.createdatetime' => 'CreateDateTime',
+    'tags.management' => 'Management',
+    'tags.edit' => 'Edit',
+    'tags.delete' => 'Delete',
+    'tags.delete.sure' => 'Sure you want to delete it?',
+    'tags.modify' => 'Modify',
+    'tags.modify.success' => 'Modify tag successfully',
+);
---
+++ resources/lang/zh/message.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiahu
+ * Date: 2015/4/20
+ * Time: 11:39
+ */
+return array(
+    'yes' => '确定',
+    'no' => '取消',
+    'editarticle' => '编辑文章',
+    'edittag' => '编辑标签',
+
+    'index.by' => '由',
+    'index.postedon' => '发表于',
+    'index.under' => '标签',
+
+    'topbar.name' =>'胡佳 博客',
+    'topbar.users' => '用户',
+    'topbar.articles' => '文章',
+    'topbar.tags' => '标签',
+    'topbar.login' => '登录',
+    'topbar.logout' => '退出',
+    'topbar.register' => '注册',
+    'topbar.publisharticle' => '发布文章',
+    'topbar.information' => '个人信息',
+    'topbar.myarticles' => '我的文章',
+
+    'user.edit.email'=>'邮箱:',
+    'user.edit.nickname'=>'昵称:',
+    'user.edit.oldpassword'=>'旧密码:',
+    'user.edit.newpassword'=>'新密码:',
+    'user.edit.confirmpassword'=>'确认新密码:',
+    'user.edit.modify'=>'修改',
+    'user.edit.modify.success'=>'修改成功',
+
+    'login.email' => '邮箱:',
+    'login.password' => '密码:',
+    'login.rememberme' => '记住我',
+    'login.login' => '登录',
+
+    'register.email'=>'邮箱:',
+    'register.nickname'=>'昵称:',
+    'register.password'=>'密码:',
+    'register.confirmpassword'=>'确认密码:',
+    'register.register'=>'注册',
+    'register.successs'=>'注册成功, 请登录',
+    'register.failure'=>'注册失败',
+
+    'admin.users.id' => 'ID',
+    'admin.users.email' => '邮箱',
+    'admin.users.nickname' => '昵称',
+    'admin.users.management' => '管理',
+    'admin.users.edit' => '编辑',
+    'admin.users.reset' => '重置',
+    'admin.users.reset.success' => '重置密码成功',
+    'admin.users.block' => '锁定',
+    'admin.users.block.success' => '锁定用户成功',
+    'admin.users.unblock' => '解锁',
+    'admin.users.unblock.success' => '解锁用户成功',
+    'admin.users.reset.sure' => '确定将密码重置为123456？',
+    'admin.users.lock.sure' => '确定锁定该用户？',
+    'admin.users.unlock.sure' => '确定解锁该用户？',
+
+    'articles.title' => '标题',
+    'articles.tags' => '标签',
+    'articles.tags.hint' => '多个标签用","分开',
+    'articles.content' => '内容',
+    'articles.management' => '管理',
+    'articles.edit' => '编辑',
+    'articles.delete' => '删除',
+    'articles.delete.sure' => '确定删除该文章?',
+    'articles.preview' => '预览',
+    'articles.modify' => '修改',
+    'articles.publish' => '发布',
+    'articles.author' => '作者',
+    'articles.datetime' => '日期',
+
+    'tags' => '标签',
+    'tags.alltags' => '所有标签',
+    'tags.tagname' => '标签名',
+    'tags.articlecount' => '文章数量',
+    'tags.createdatetime' => '创建时间',
+    'tags.management' => '管理',
+    'tags.edit' => '编辑',
+    'tags.delete' => '删除',
+    'tags.delete.sure' => '确定删除该标签？',
+    'tags.modify' => '修改',
+    'tags.modify.success' => '修改标签成功',
+);
