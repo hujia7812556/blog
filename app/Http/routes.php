089c007d3d27fb3b70e3776e601e5f6af01f4963
jiahu <jiahu@pptv.com>
2015-06-11 18:14:26 +0800

    去除子域名

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,38 +20,36 @@
 //	'password' => 'Auth\PasswordController',
 //]);
 
-Route::group(array('domain'=>'jerehu.com'), function() {
-    Route::get('/','IndexController@index');
-    Route::get('auth/login',array('as'=>'login','uses'=>'LoginController@index'));
-    Route::post('auth/login', array('as'=>'login', 'uses'=>'LoginController@store'));
-    Route::get('home', array('middleware' => 'auth', 'uses' => 'LoginController@showHome'));//用户主页
-    Route::get('auth/logout', array('as'=>'logout',function () {
-        Auth::logout();
-        return Redirect::to('/');
-    }));
+Route::get('/','IndexController@index');
+Route::get('auth/login',array('as'=>'login','uses'=>'LoginController@index'));
+Route::post('auth/login', array('as'=>'login', 'uses'=>'LoginController@store'));
+Route::get('home', array('middleware' => 'auth', 'uses' => 'LoginController@showHome'));//用户主页
+Route::get('auth/logout', array('as'=>'logout',function () {
+    Auth::logout();
+    return Redirect::to('/');
+}));
 
-    Route::get('register', 'RegisterController@index');
-    Route::post('register', array('uses' => 'RegisterController@store'));
-    Route::get('user/{id}/edit', array('middleware' => 'auth', 'as' => 'user.edit', 'uses' => 'UserController@edit'));
-    Route::put('user/{id}', array('middleware' => 'auth', 'uses' => 'UserController@update'));
+Route::get('register', 'RegisterController@index');
+Route::post('register', array('uses' => 'RegisterController@store'));
+Route::get('user/{id}/edit', array('middleware' => 'auth', 'as' => 'user.edit', 'uses' => 'UserController@edit'));
+Route::put('user/{id}', array('middleware' => 'auth', 'uses' => 'UserController@update'));
 
-    Route::group(array('prefix' => 'admin', 'middleware' => array('auth','isAdmin')), function () {
-        Route::get('users', 'AdminController@users');
-        Route::get('articles', 'AdminController@articles');
-        Route::get('tags', 'AdminController@tags');
-    });
-
-    Route::group(array('before' => 'auth|csrf|isAdmin'), function () {
-        Route::put('user/{user}/reset', 'AdminController@resetUser');
-        Route::delete('user/{user}', 'AdminController@blockUser');
-        Route::put('user/{user}/unblock', 'AdminController@unblockUser');
-    });
-
-    Route::resource('article', 'ArticleController');
-    Route::post('article/preview', array('middleware' => 'auth', 'uses' => 'ArticleController@preview'));
-    Route::get('user/{user}/articles', 'UserController@articles');
-    Route::post('article/{id}/preview', array('middleware' => 'auth', 'uses' => 'ArticleController@preview'));
-    Route::resource('tag', 'TagController');
-    Route::get('tag/{id}/articles', 'TagController@articles');
-    Route::get('tag/list', array('as'=>'tag_list','uses'=>'TagController@show'));
+Route::group(array('prefix' => 'admin', 'middleware' => array('auth','isAdmin')), function () {
+    Route::get('users', 'AdminController@users');
+    Route::get('articles', 'AdminController@articles');
+    Route::get('tags', 'AdminController@tags');
 });
+
+Route::group(array('before' => 'auth|csrf|isAdmin'), function () {
+    Route::put('user/{user}/reset', 'AdminController@resetUser');
+    Route::delete('user/{user}', 'AdminController@blockUser');
+    Route::put('user/{user}/unblock', 'AdminController@unblockUser');
+});
+
+Route::resource('article', 'ArticleController');
+Route::post('article/preview', array('middleware' => 'auth', 'uses' => 'ArticleController@preview'));
+Route::get('user/{user}/articles', 'UserController@articles');
+Route::post('article/{id}/preview', array('middleware' => 'auth', 'uses' => 'ArticleController@preview'));
+Route::resource('tag', 'TagController');
+Route::get('tag/{id}/articles', 'TagController@articles');
+Route::get('tag/list', array('as'=>'tag_list','uses'=>'TagController@show'));
